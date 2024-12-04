cb547e1dbab2a75bcdeefba25cb7495806692e00
Sofiia Khemii <sofiahemij@outlook.com>
2024-12-04 20:33:33 +0200

    update models

--- TaskTraker/TaskTraker.Data/Models/Board.cs
+++ TaskTraker/TaskTraker.Data/Models/Board.cs
@@ -6,7 +6,7 @@
 
         public string? Name { get; set; }
 
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         public ICollection<TaskItem>? Tasks { get; set; }
 
--- TaskTraker/TaskTraker.Data/Models/TaskItem.cs
+++ TaskTraker/TaskTraker.Data/Models/TaskItem.cs
@@ -8,7 +8,7 @@
 
         public string? Description { get; set; }
 
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         public int BoardId { get; set; }
 
